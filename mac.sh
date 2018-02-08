10c40c6929d89439dd30622e9bd450746d0ac031
Emmanuel BUU <ebuu@Mac-mini-de-ives.local>
2018-02-08 16:11:24 +0100

    ajout script compil

---
+++ mac.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+./configure --enable-pic --enable-static --disable-swscale --prefix=$HOME/doubango/doubango/thirdparties/mac --libdir=$HOME/doubango/doubango/thirdparties/mac/lib/x86_64
+make
